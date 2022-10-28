554d8ac121e949f3738baeaf54d024273d1cf9f4
Mariane Henriques <mariane.henriques@outlook.com>
2022-10-28 13:48:49 -0300

    alteração home para os padrões do cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,7 +2,7 @@
 
 describe('page home', ()=>{
     it('app deve esta onile', ()=> {
-        cy.visit('https://buger-eats.vercel.app/')
+        cy.visit('/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
 
 
